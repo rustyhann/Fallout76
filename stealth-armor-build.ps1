a16565fbf4423cd3ab238c8269c1d748db363f20
Rusty Hann <rustyhann@gmail.com>
2020-06-19 09:07:04 -0500

    initial commit

---
+++ stealth-armor-build.ps1
@@ -0,0 +1,16 @@
+https://nukesdragons.com/fallout76/perks?v=1&s=f1545fb&d=s92se2sf2aj2an2a04l71lt2lu2c72es2eg1sg2p10sb2s72lb2ao0i44&m=c98ghe&wp=w0&wm=1a2133
+
+adamantium skeleton and travel agent
+https://nukesdragons.com/fallout76/perks?v=1&s=f1855bb&d=s92se2sf2aj2a04l71lt2lu2c72es2eg1sg2p10sb2s72lb2i44cr0eo2&m=c98ghe&wp=w0&wm=1a2133
+
+4
+
+AGI - ACTION POINTS
+AGI - SPRINTING REQUIRES FEWER ACTION POINTS
+AGI - ESCAPE TO RUN 1
+AGI - SNEAKING TRAPS 1
+
+END - CHEMS GENERATE NO THIRST 2
+END - CHEMS GENERATE NO HUNGER 2
+
+CHA - FIELD SURGEON FASTER STIMPAKS
